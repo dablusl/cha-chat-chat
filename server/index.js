a451d2c0f657a196d9e1f1b423c35da5bda1be04
dablusl <127994387+dablusl@users.noreply.github.com>
2023-09-22 14:33:02 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import http from 'http';
+import { Server as SocketServer } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+
+  socket.on('message', (body) => {
+    socket.broadcast.emit('message', {
+      //store message in database
+
+      //send message to all clients
+      body,
+      from: 'other person',
+    })
+  });
+});
+ 
+server.listen(3000);
+
+console.log('listening on port', 3000);
