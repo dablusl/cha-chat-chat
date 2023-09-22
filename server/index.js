d6e4d137f186b27a00c43c32c61eab7c1d0c191d
dablusl <127994387+dablusl@users.noreply.github.com>
2023-09-22 15:19:43 -0500

    adding console messages for debugging

--- server/index.js
+++ server/index.js
@@ -6,16 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
 
-const PORT = process.env.PORT || 3000; // Use the PORT environment variable if available, or default to 3000
+const PORT = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
   console.log('Client connected');
 
   socket.on('message', (body) => {
+    console.log('Received message:', body);
     socket.broadcast.emit('message', {
-      //store message in database
-
-      //send message to all clients
       body,
       from: 'other person',
     });
